c484de05169e442942ddd511065ff0b39ff0daa8
Fabio <fabio.7.45@hotmail.com>
2023-12-02 17:48:34 -0300

    enumerator

---
+++ ITARATOR.py
@@ -0,0 +1,13 @@
+"""
+ENUMERATE - ENUMERA ITERAVEIS (INDICE)
+    
+"""
+lista = ['maria','Patricia','JoseFabio']
+lista . append('Miguel')
+
+lista_enumerada = list(enumerate(lista))
+
+print(lista_enumerada)
+
+
+
